a13593c10bde3f9603721f0f3387a75007272616
DESKTOP-8GT80MQ\User <jungmin.kim@partridgesystems.com>
2023-05-03 17:52:03 +0900

    style : 초기 세팅 커밋

---
+++ testToy/src/main/java/com/example/testtoy/TestToyApplication.java
@@ -0,0 +1,13 @@
+package com.example.testtoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestToyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestToyApplication.class, args);
+    }
+
+}
---
+++ testToy/src/main/java/com/example/testtoy/domain/user/User.java
@@ -0,0 +1,5 @@
+package com.example.testtoy.domain.user;
+
+public class User {
+    
+}
