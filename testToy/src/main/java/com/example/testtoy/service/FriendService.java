397b52ec7da941b233307a5e93791c2d73f96f9b
DESKTOP-8GT80MQ\User <jungmin.kim@partridgesystems.com>
2023-05-09 11:36:02 +0900

    fix : 친구 요청 및 수락 작업 전 세팅

--- testToy/src/main/java/com/example/testtoy/service/FriendService.java
+++ testToy/src/main/java/com/example/testtoy/service/FriendService.java
@@ -61,21 +61,35 @@ public class FriendService {
 
         if(sender.equals(receiver)){
             // 자기 자신에게 친구 요청을 보낸 경우,
+
         }
 
 
         /*     로직2     */
+
+        // 경우의수 1)이미 친구 2)친구 요청을 보낸 상태 3)친구 요청을 받은 상태 4)요청을 보내지도, 받지도 않은 상태
+
+        // 1)이미 친구인 경우,
         if (sender.getFriends1().contains(receiver) && sender.getFriends2().contains(receiver)) {
             throw new RuntimeException("해당 사용자와 이미 친구입니다.");
         }
 
+        // 2)친구 요청을 보낸 상태,
+        if (sender.getSentFriendRequests().stream().anyMatch(fr -> fr.getReceiver().equals(receiver))) {
+            throw new RuntimeException("이미 해당 사용자에게 친구 요청을 보냈습니다.");
+        }
+
+        // 3)친구 요청을 받은 상태,
+        // receiver가 받은 친구 요청 목록 중에서, sender가 발신한 친구 요청이 존재하는지 확인
+        // receiver가 이미 sender로부터 친구 요청을 받았는지 여부 확인
         if (receiver.getReceivedFriendRequests().stream().anyMatch(fr -> fr.getSender().equals(sender))) {
             throw new RuntimeException("이미 해당 사용자로부터 친구 요청을 받았습니다.");
         }
 
-        if (sender.getSentFriendRequests().stream().anyMatch(fr -> fr.getReceiver().equals(receiver))) {
-            throw new RuntimeException("이미 해당 사용자에게 친구 요청을 보냈습니다.");
-        }
+        // 4)친구 요청을 보내지도, 받지도 않은 상태
+
+
+
 
 
 
--- testToy/src/test/java/com/example/testtoy/service/MemberServiceTest.java
+++ testToy/src/test/java/com/example/testtoy/service/MemberServiceTest.java
@@ -24,8 +24,8 @@ class MemberServiceTest {
     @Test
     public void 회원가입() throws Exception{
         //given
-        Member member = new Member();
-        member.setName("Kim");
+        //Member member = new Member();
+        //member.setName("Kim");
 
         //when
         //Long saveId = memberService.join(member);
@@ -38,11 +38,11 @@ class MemberServiceTest {
     @Test
     public void 중복_회원_예외() throws Exception {
         //given
-        Member member1 = new Member();
-        member1.setName("kim");
+        //Member member1 = new Member();
+        //member1.setName("kim");
 
-        Member member2 = new Member();
-        member2.setName("kim");
+        //Member member2 = new Member();
+        //member2.setName("kim");
 
         //when
         //memberService.join(member1);
