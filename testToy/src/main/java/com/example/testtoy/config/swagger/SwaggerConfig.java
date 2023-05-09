d79bf42ecf4fd08410d18c822dc602efb8f2de12
DESKTOP-8GT80MQ\User <jungmin.kim@partridgesystems.com>
2023-05-09 11:55:18 +0900

    feat : Swagger 추가

---
+++ testToy/src/main/java/com/example/testtoy/config/swagger/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.example.testtoy.config.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket restAPI() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("파트리지시스템즈 토이프로젝트 REST API")
+                .version("1.0.0")
+                .description("파트리지시스템즈 토이 프로젝트의 swagger api 입니다.")
+                .build();
+    }
+}
