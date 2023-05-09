430ef092049407751daa4c9b80d337c9faff3e90
DESKTOP-8GT80MQ\User <jungmin.kim@partridgesystems.com>
2023-05-09 17:58:38 +0900

    fix : 친구 관계 확인 로직 수정

--- testToy/src/main/java/com/example/testtoy/domain/member/Member.java
+++ testToy/src/main/java/com/example/testtoy/domain/member/Member.java
@@ -53,4 +53,5 @@ public class Member {
                 .password(password)
                 .build();
     }
+
 }
--- testToy/src/main/java/com/example/testtoy/service/FriendService.java
+++ testToy/src/main/java/com/example/testtoy/service/FriendService.java
@@ -34,89 +34,19 @@ public class FriendService {
         // 친구 요청을 받는 유저
         Member receiver = memberRepository.findOne(receiverId);
 
-        /*     로직1     */
-/*
-        // sender가 receiver에게 이미 친구 요청을 한 경우,
-        if(friendRequestRepository.findBySenderAndReceiver(sender, receiver).isPresent()){
+        FriendRequest fromSenderToReceiver = friendRequestRepository.existsFriendRequestsBySender_IdAndAndReceiver_Id(senderId,receiverId);
+        FriendRequest fromReceiverToSender = friendRequestRepository.existsFriendRequestsBySender_IdAndAndReceiver_Id(receiverId,senderId);
+        System.out.println("fromSenderToReceiver : " + fromSenderToReceiver + " , fromReceiverToSender : " + fromReceiverToSender);
 
-            System.out.println("이미 친구 요청을 한 상태!");
-
-            // 친구 수락 작업 수행
-            Optional<FriendRequest> friendRequest = friendRequestRepository.findBySenderAndReceiver(sender,receiver);
-
-            // 친구 처리
-            friendRequest.get().setState(FriendStatus.FRIEND);
+        if(fromSenderToReceiver!=null && fromReceiverToSender!=null){
+            System.out.println("친구 관계!");
+        } else if (fromSenderToReceiver!=null && fromReceiverToSender==null) {
+            System.out.println("이미 친구 요청을 보냈습니다.");
+        } else if (fromSenderToReceiver==null && fromReceiverToSender!=null) {
+            System.out.println("이미 친구 요청을 받은 상태입니다.");
+        } else{
+            System.out.println("서로 간 친구 요청 내역 없음.");
         }
-        // sender가 receiver에게 친구 요청을 하지 않은 경우,
-        else{
-            System.out.println("친구 요청을 한 적이 없음!");
-            // 친구 요청 작업 수행
-            FriendRequest friendRequest = new FriendRequest();
-            friendRequest.setSender(sender);
-            friendRequest.setReceiver(receiver);
-            friendRequest.setState(FriendStatus.REQUEST);
-            friendRequestRepository.save(friendRequest);
-        }
-*/
-
-
-
-        if(sender.equals(receiver)){
-            // 자기 자신에게 친구 요청을 보낸 경우,
-            System.out.println("자기자신에게 친구 요청을 전송");
-        }
-
-
-        /*     로직2     */
-
-        // 경우의수 1)이미 친구 2)친구 요청을 보낸 상태 3)친구 요청을 받은 상태 4)요청을 보내지도, 받지도 않은 상태
-
-        // 1)이미 친구인 경우,
-
-        if (sender.getFriends().stream().anyMatch(friend1 -> friend1.getFriendMember().getId().equals(receiver.getId()))
-                && receiver.getFriends().stream().anyMatch(friend2 -> friend2.getFriendMember().getId().equals(sender.getId()))) {
-
-            System.out.println("이미 친구. receiver : " + receiver.getId() + " , sender : " + sender.getId());
-            throw new RuntimeException("해당 사용자와 이미 친구입니다.");
-        }
-
-
-        System.out.println("바로 위 쿼리는 이미 친구인 경우 확인");
-
-        // 2)친구 요청을 보낸 상태,
-        if (sender.getSentFriendRequests().stream().anyMatch(request -> request.getReceiver().equals(receiver))) {
-            System.out.println("이미 친구 요청 보냄. receiver : " + receiver.getId() + " , sender : " + sender.getId());
-            throw new RuntimeException("이미 해당 사용자에게 친구 요청을 보냈습니다.");
-        }
-
-        System.out.println("바로 위 쿼리는 이미 친구 요청을 보낸 경우 확인");
-
-        // 3)친구 요청을 받은 상태,
-        // receiver가 받은 친구 요청 목록 중에서, sender가 발신한 친구 요청이 존재하는지 확인
-        // receiver가 이미 sender로부터 친구 요청을 받았는지 여부 확인
-        System.out.println("receiver 확인 : " + receiver.getName() + ", sender 확인 : " + sender.getName());
-        System.out.println("receiver id : "+ receiver.getId()+ " sender id : " + sender.getId());
-
-
-
-        if (sender.getReceivedFriendRequests().stream().anyMatch(request -> request.getReceiver().equals(sender))) {
-            // 친구 요청 수락 및 친구 관계 맺기 작업 수행
-            System.out.println("이미 receiver로부터 친구 요청을 받음. receiver : " + receiver.getId() + " , sender : " + sender.getId());
-            throw new RuntimeException("이미 해당 사용자로부터 친구 요청을 받았습니다.");
-        }
-
-
-        System.out.println("바로 위 쿼리는 이미 친구 요청을 받은 경우 확인");
-
-        // 4)친구 요청을 보내지도, 받지도 않은 상태
-        // 친구 요청 전송 작업 수행
-        FriendRequest friendRequest = FriendRequest.createFriendRequest(sender, receiver, FriendStatus.REQUEST);
-
-        friendRequestRepository.save(friendRequest);
-
-
-
-
 
 
         return ResponseEntity.ok(200);
